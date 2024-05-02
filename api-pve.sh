de05d8af11970a41a3b966d442c5f8f4e6fa9c22
Yann-T <yanntejero@gmail.com>
2024-05-02 17:35:48 +0200

    Fix

--- api-pve.sh
+++ api-pve.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
-
-sudo apt install jq -y
+echo "If you don't installed jq, you should. Otherwise this script won't work."
 
 # --------------------- #
 #Variables with prompts
@@ -9,23 +7,25 @@ sudo apt install jq -y
 
 #Username
 read -p "Please enter the username: " username
+echo
 
 #Password
 read -sp "Password: " password
+echo
 
 #Proxmox address
 read -p "Proxmox IP address: " proxmox
-
+echo
 
 # ---------------------- #
 # Authentication tokens
 # ---------------------- #
 
 #cookie
-curl --silent --insecure --data "username=$username@pam&password=$password" $proxmox:8006/api2/json/access/ticket | jq --raw-output '.data.ticket' | sed 's/^/PVEAuthCookie=/' > cookie
+curl --silent --insecure --data "username=$username@pam&password=$password" https://$proxmox:8006/api2/json/access/ticket | jq --raw-output '.data.ticket' | sed 's/^/PVEAuthCookie=/' > cookie
 
 # csrftoken
-curl --silent --insecure --data "username=$username@pam&password=$password" $proxmox:8006/api2/json/access/ticket | jq --raw-output '.data.CSRFPreventionToken' | sed 's/^/CSRFPreventionToken:/' > csrftoken
+curl --silent --insecure --data "username=$username@pam&password=$password" https://$proxmox:8006/api2/json/access/ticket | jq --raw-output '.data.CSRFPreventionToken' | sed 's/^/CSRFPreventionToken:/' > csrftoken
 
 
-curl --silent --insecure --cookie "$(<cookie)" --header "$(<csrftoken)" $proxmox:8006/api2/nodes | jq .
+curl --silent --insecure --cookie "cookie" --header "csrftoken" https://$proxmox:8006/api2/nodes | jq .
